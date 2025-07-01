495fc172730a46f9f72096d2f48ef4a63251c76a
Anita Samuel <migist2@gmail.com>
2025-07-01 23:09:39 +0100

    responsive css grid

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ["./src/**/*.{html,js}"],
+    content: [
+    "./*.html",              // Scan HTML files in root directory (your 1-index.html)
+    "./src/**/*.{html,js}"   // Also scan src folder
+  ],
   theme: {
     extend: {},
   },
